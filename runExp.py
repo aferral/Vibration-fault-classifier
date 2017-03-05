b8e97780f5a48dae1bff3aed669fd71cee574c10
aferral <andreslago5@hotmail.com>
2017-03-05 13:56:17 -0300

    -Improved dataset random split -Early stop if accuracy get above certain value (train) -I didnt add the full dataset to the git to keep it lightweight (i will be adding download links)

--- runExp.py
+++ runExp.py
@@ -19,7 +19,7 @@ for data in datasets:
         alt = bool(i)
 
         now = time.strftime('-day%Y-%m-%d-time%H-%M')
-        nb_kwargs = {'dataFolder': dFolder, 'alternativeArc': alt, 'timeNow': now, 'epochs' : 20}
+        nb_kwargs = {'dataFolder': dFolder, 'alternativeArc': alt, 'timeNow': now, 'epochs' : 1}
 
         dname = nb_kwargs['dataFolder'].split("/")[1]
         alt2 = str(nb_kwargs['alternativeArc'])
@@ -27,3 +27,5 @@ for data in datasets:
         nbrun.run_notebook(note,out_path=outReport,timeout=10000000,
                            nb_suffix='-out_%s_%s--%s' % (dname,str(alt2),str(now)),
                            nb_kwargs=nb_kwargs,execute_kwargs={"kernel_name": 'python2' })
+
+        break
